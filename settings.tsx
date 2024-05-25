86a9cea7c1f4788880fec88d1e38f3a88cfe5e4d
Brr0 <risa6550@seznam.cz>
2024-05-25 17:32:35 +0200

    first commit

--- settings.tsx
+++ settings.tsx
@@ -2,7 +2,7 @@
 // label has to be the exact same as in your candy machine config
 export const mintText = [
   { label: "1", mintText: "Mint price: 0.03sol + TX fees", buttonLabel: "Mint now!", header: "Public mint" },
-  { label: "2", mintText: "Mint price: xx$KDNA + Mint Token", buttonLabel: "Mint now!", header: "$KDNA mint" },
+  { label: "2", mintText: "Mint price: 1000$KDNA + Mint Token", buttonLabel: "Mint now!", header: "$KDNA mint" },
  ];
 
 //header image in the ui. replace with your own
